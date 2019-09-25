c6c730b4bd30b56343fb09dbbaf50819e8541520
Danny Yuxie <luckindan@gmail.com>
2019-09-24 23:11:30 -0400

    Draf updated

---
+++ Strassen/matrx.cpp
@@ -0,0 +1,15 @@
+class matrx{
+    public:
+
+    matrx(){};
+    void retrieve_info(string filename){};
+    bool reg_mul(){};
+    bool stras_mul(){};
+    int get_stras_space(){}; 
+    int get_stras_time(){}; 
+    int get_reg_space(){}; 
+    int get_reg_time(){}; 
+    
+    private:
+
+};
