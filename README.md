fc6ce2d4164a914958e78308fd2621ef43963ada
luckindan <luckindan@gmail.com>
2019-10-03 10:42:09 -0400

    Readme updated

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # strassen_matrix_multiplication
+Dependencies:
+\tGenerating data: Python 3.6.7
+\tComputer matrices: Standard c++ library
+
+
+Compiling Program on Linux:
+	g++ matrx.cpp main.cpp -o out
+
+Run Data Generation:
+	python3 craete_data.py <row> <col> <max_value> <file_name>
+	for help:
+		python3 create_data.py -h 
--- Strassen/data/create_data.py
+++ Strassen/data/create_data.py
@@ -3,23 +3,27 @@ import argparse
 import math
 import sys
 
-def create_data(x, num, filename):
+def create_data(row, col, num, filename):
 	filename = str(filename)
-	x = int(x)
+	row = int(row)
+	col = int(col)
 	num = int(num)
+
 	with open(str(filename), 'w') as f:
-		for i in range(x):
+		for i in range(row):
 			a = []
-			for j in range(x):
+			for j in range(col):
 				a.append(str(int(random.randrange(0,num))))
 			f.write(', '.join(a) + '\n')
 
 
 if __name__ ==  '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument("matrix_size", help = "the number of row and col", default=100)
+	parser.add_argument("row", help = "the number of row", default=100)
+	parser.add_argument("col", help = "the number of col", default=100)
+	
 	parser.add_argument("max_value", help="The max random value",default=math.sqrt(sys.maxsize)-1)
 	parser.add_argument("filename", help="File name where to store data", default='random_data.txt')
 
 	args = parser.parse_args()
-	create_data(args.matrix_size, args.max_value, args.filename)
+	create_data(args.row, args.col , args.max_value, args.filename)
