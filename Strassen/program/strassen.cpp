948349e1ba59f93ca6c543bbeb92f271f8ae930d
luckindan <luckindan@gmail.com>
2019-10-07 21:38:18 -0400

    Compiling issue fixed

--- Strassen/program/strassen.cpp
+++ Strassen/program/strassen.cpp
@@ -1,4 +1,33 @@
 #include "matrx.h"
+vec matrix2x2(vec &data1, vec &data2) {
+	vec ans;
+
+	int a = data1[0][0];
+	int b = data1[0][1];
+	int c = data1[1][0];
+	int d = data1[1][1];
+	int e = data2[0][0];
+	int f = data2[0][1];
+	int g = data2[1][0];
+	int h = data2[1][1];
+	int p1 = a * (f - h);
+	int p2 = (a + b) * h;
+	int p3 = (c + d) * e;
+	int p4 = d * (g - e);
+	int p5 = (a + d) * (e + h);
+	int p6 = (b - d) * (g + h);
+	int p7 = (a - c) * (e - f);
+	vector<int> tempA;
+	vector<int> tempB;
+	tempA.push_back(p5 + p4 - p3 + p6);
+	tempA.push_back(p1 + p2);
+	ans.push_back(tempA);
+	tempB.push_back(p3 + p4);
+	tempB.push_back(p1 + p5 + p7 - p3);
+	ans.push_back(tempB);
+
+	return ans;
+}
 
 vec strassen(vec &data1, vec &data2){
 	vec ans;
@@ -21,13 +50,12 @@ vec strassen(vec &data1, vec &data2){
 
 	std::cout << "strassen" << std::endl;
 	
-	recursive(data1, data2, letter);
+	//recursive(data1, data2, letter);
 
 	return ans;
 }
 
-//not finished
-int recursive(vec &data1, vec &data2, vec &letter) {
+/*int recursive(vec &data1, vec &data2, vec &letter) {
 	if (letter.size == 1) {
 		letter[0];
 	}
@@ -58,32 +86,5 @@ int recursive(vec &data1, vec &data2, vec &letter) {
 	int p5 = (a + d) * (e + h);
 	int p6 = (b - d) * (g + h);
 	int p7 = (a - c) * (e - f);
-}
+}*/
 
-vec matrix2x2(vec &data1, vec &data2) {
-	vec ans;
-
-	int a = data1[0][0];
-	int b = data1[0][1];
-	int c = data1[1][0];
-	int d = data1[1][1];
-	int e = data2[0][0];
-	int f = data2[0][1];
-	int g = data2[1][0];
-	int h = data2[1][1];
-	int p1 = a * (f - h);
-	int p2 = (a + b) * h;
-	int p3 = (c + d) * e;
-	int p4 = d * (g - e);
-	int p5 = (a + d) * (e + h);
-	int p6 = (b - d) * (g + h);
-	int p7 = (a - c) * (e - f);
-	vector<int> tempA;
-	vector<int> tempB;
-	tempA.push_back(p5 + p4 - p3 + p6);
-	tempA.push_back(p1 + p2);
-	ans.push_back(tempA);
-	tempB.push_back(p3 + p4);
-	tempB.push_back(p1 + p5 + p7 - p3);
-	ans.push_back(tempB);
-}
