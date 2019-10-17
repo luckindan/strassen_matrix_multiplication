41f82efcae392993b33172d8c95ebb23c900ffea
luckindan <luckindan@gmail.com>
2019-10-17 12:22:57 -0400

    Second update

--- Strassen/program/reg_compute.cpp
+++ Strassen/program/reg_compute.cpp
@@ -1,26 +1,31 @@
 
 #include "matrx.h"
-
+#include <exception>
 vec reg_compute(vec &data1, vec &data2){
 
 	vec ans;
 	int temp;
 	std::cout << "reg_compute" << std::endl;
-
-	for (int i=0; i<data1.size();i++){
-		vector<int> tempV;
-		for(int j=0; j<data2[0].size();j++){
-			temp=0;
-			for(int k=0;k<data1[0].size();k++){
-				temp+=(data1[i][k]*data2[k][j]);
-			}
-			tempV.push_back(temp);
+	try{
+		if (data1[0].size()!= data2.size()){
+			throw "Invalid matrices";
 		}
-		ans.push_back(tempV);
+		for (int i=0; i<data1.size();i++){
+			vector<int> tempV;
+			for(int j=0; j<data2[0].size();j++){
+				temp=0;
+				for(int k=0;k<data1[0].size();k++){
+					temp+=(data1[i][k]*data2[k][j]);
+				}
+				tempV.push_back(temp);
+			}
+			ans.push_back(tempV);
+		}
 	}
-	/*
-		Implementation goes here
-	*/
+	catch(string err){
+		cout << err << endl;
+		exit(1);
+	}
 
 
 	return ans;
