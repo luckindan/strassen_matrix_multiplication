045126fb1bab848af4b60d24759c261e0a71dc5d
Kristen loo <kristenloo2468@gmail.com>
2019-11-11 21:51:24 -0500

    Bug fixed

--- Strassen/program/strassen.cpp
+++ Strassen/program/strassen.cpp
@@ -152,7 +152,6 @@ vec recurse(vec data1, vec data2, pair<int,int> &space) {
 	return ans;
 }
 
-
 void fill_matx(vec &data){
 
 	
@@ -177,6 +176,7 @@ void fill_matx(vec &data){
 		data.push_back(vector<int> (q, 0));
 	}
 }
+
 //main
 vec strassen(vec &data1, vec &data2, pair<int,int> &space){
 	vec ans;
